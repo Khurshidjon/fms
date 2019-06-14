3acf9fc07019918313a9ee0f67e4ed204e8fc6ae
xurshid <mr.xurshidjon1995@gmail.com>
2019-06-14 12:11:05 +0500

    translate

--- resources/lang/en/pages.php
+++ resources/lang/en/pages.php
@@ -3,6 +3,31 @@
 return [
 
     'back_to_list' => 'Back to list',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'continue' => 'Продолжить',
+    'back_to_home' => 'Вернуться на главную страницу',
+    'back_to_prev' => 'Назад к предыдущему',
+    'select_one' => 'Выберите',
+    'from_city' => 'Из города',
+    'from_district' => 'Из района',
+    'sender_fio' => 'Отправитель ФИО',
+    'sender_phone' => 'Телефон отправителя',
+    'sender_address' => 'Адрес отправителя',
+    'sender_org_name' => 'Орган отправителя',
+    'from_date' => 'С даты',
+    'to_city' => 'В город',
+    'to_district' => 'В район',
+    'receiver_fio' => 'Получатель ФИО',
+    'receiver_phone' => 'Телефон получателя',
+    'receiver_address' => 'Адрес получателя',
+    'receiver_org_name' => 'Орган получателя',
+    'with_courier' => 'С курьером',
+    'with_delivery' => 'С доставкой',
+    'pieces' => 'Количество',
+    'weight' => 'Вес',
+    'volume' => 'Объем',
+    'product_category' => 'Категория продукта',
+    'contract_number' => 'Контактный номер',
+    'type_pay_for_service' => 'Категория оплаты',
+
 
 ];
