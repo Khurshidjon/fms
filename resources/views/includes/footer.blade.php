04eeae47de183258973cc5d6a4e9077c9b0d23f0
xurshid <mr.xurshidjon1995@gmail.com>
2019-08-04 10:37:53 +0500

    login page changed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -52,7 +52,7 @@ $lang = App::getLocale();
 
   <div class="container">
     <div class="copyright">
-       <strong>fayl mail service</strong>
+       <strong>fly mail service</strong>
     </div>
     <div class="credits">
       
